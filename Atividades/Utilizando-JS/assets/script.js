c965af106b509b523d9bd7ce3e3e40f591b0bd53
3LieLs <3lielfernandesribeiro@gmail.com>
2023-11-29 10:26:46 -0300

    att

--- Atividades/Utilizando-JS/assets/script.js
+++ Atividades/Utilizando-JS/assets/script.js
@@ -1,25 +1,38 @@
 var aumentar = false
 var boxVal = window.document.querySelector('div#box')
 var runnerVal = window.document.querySelector('div#runner')
+var tamanhoVal = window.document.querySelector('div#tamanho')
 
 
 boxVal.addEventListener('click', changeColor)
 runnerVal.addEventListener('mouseover', dislocar)
+tamanhoVal.addEventListener('click', aumentarDiminuir)
 
 
 function changeColor() {
-    
-    box.style.background = 'orange'
-    box.style.width = '200px'
-    box.style.height = '200px'
+    let cor1, cor2, cor3
 
-    //document.getElementById("box").style = "background-color :orange"
+    cor1 = Math.floor(Math.random(0) * 255);
+    cor2 = Math.floor(Math.random(0) * 255);
+    cor3 = Math.floor(Math.random(0) * 255);
+
+    box.style.background = `rgb(${cor1}, ${cor2}, ${cor3})`
 }
+
 function dislocar() {
-    let aleatorio1, aleatorio2 
+    let aleatorio1, aleatorio2
 
     aleatorio1 = Math.floor(Math.random(0) * 1000);
     aleatorio2 = Math.floor(Math.random(0) * 500);
-    
+
     runner.style = `left: ${aleatorio1}px; top: ${aleatorio2}px;`
+}
+
+function aumentarDiminuir() {
+    let altura, comprimento
+
+    altura = Math.floor(Math.random(50) * 200);
+    comprimento = Math.floor(Math.random(50) * 200);
+
+    tamanho.style = `width: ${comprimento}px; height: ${altura}px;`
 }
